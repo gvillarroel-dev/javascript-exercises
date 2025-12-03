e6f6cca0e1f6ab20f6e7511237fb303a5189ba57
Giuliana Villarroel <giu.villarroel@gmail.com>
2025-12-03 19:21:52 -0300

    Complete sumAll function

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,24 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+	if (
+		num1 < 0 ||
+		num2 < 0 ||
+		!Number.isInteger(num1) ||
+		!Number.isInteger(num2)
+	) {
+		return "ERROR";
+	}
 
+	let min = num1 < num2 ? num1 : num2; // can also use Math.min(num1, num2)
+	let max = num1 > num2 ? num1 : num2; // can also use Math.max(num1, num2)
+	let acc = 0;
+
+	for (let i = min; i <= max; i++) {
+		acc += i;
+	}
+	return acc;
 };
+
+console.log(sumAll(4, 1));
 
 // Do not edit below this line
 module.exports = sumAll;
