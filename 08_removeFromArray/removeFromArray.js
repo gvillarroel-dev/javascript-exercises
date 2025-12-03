dc5032bbd062335f6853380ab795ecc6179cbde1
Giuliana Villarroel <giu.villarroel@gmail.com>
2025-12-03 18:54:31 -0300

    Complete removeFromArray function

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...args) {
+	let newArray = [];
+
+	array.map((item) => {
+		// if the element is NOT in the passed arguments, then it is added to the new array
+		if (!args.includes(item)) {
+			newArray.push(item);
+		}
+	});
+
+	return newArray;
 };
+
+console.log(removeFromArray([1, 2, 3, 4], 3));
 
 // Do not edit below this line
 module.exports = removeFromArray;
