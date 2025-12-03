ff088cff96ed1cba8f1fe45b7b630959b9e60b1e
Giuliana Villarroel <giu.villarroel@gmail.com>
2025-12-03 18:39:04 -0300

    Complete reverseString function

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+	let wordReversed = "";
+	for (let i = string.length - 1; i >= 0; i--) {
+		wordReversed += string[i];
+	}
+	return wordReversed;
 };
+
+console.log(reverseString("hello world"));
 
 // Do not edit below this line
 module.exports = reverseString;
