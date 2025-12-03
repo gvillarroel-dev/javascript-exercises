b7cbd7c5998dd2e0ff29cb146bfa20889e64ab4e
Giuliana Villarroel <giu.villarroel@gmail.com>
2025-12-03 15:19:12 -0300

    feat: complete repeatString exercise

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
+const repeatString = function (str, num) {
+	if (num < 0) return "ERROR";
 
+	let repeatStr = "";
+	for (let i = 0; i < num; i++) {
+		repeatStr += str;
+	}
+	return repeatStr;
 };
+
+console.log(repeatString("hey", 3));
 
 // Do not edit below this line
 module.exports = repeatString;
