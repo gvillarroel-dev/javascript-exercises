8910c8a076ba5a7a108b79489a1963c827d93541
Giuliana Villarroel <giu.villarroel@gmail.com>
2025-12-13 20:31:26 -0300

    Complete getTheTitles function

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,5 +1,13 @@
-const getTheTitles = function() {
+const getTheTitles = function (books) {
+	// --- Solution 1: Using for...of loop ---
+	// let titlesArray = [];
+	// for (let book of books) {
+	//     titlesArray.push(book.title);
+	// }
+	// return titlesArray;
 
+	// --- Solution 2: Using map ---
+	return books.map((book) => book.title);
 };
 
 // Do not edit below this line
