b78da8996877481ee488086440e6de750e3926c4
Giuliana Villarroel <giu.villarroel@gmail.com>
2025-12-13 17:09:55 -0300

    Complete palindromes function

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,18 @@
-const palindromes = function () {
+const palindromes = function (str) {
+	const validCharacters = "abcdefghijklmnopqrstuvwxyz0123456789";
 
+	let cleanedString = str
+		.toLowerCase()
+		.split("")
+		.filter((char) => validCharacters.includes(char))
+		.join("");
+
+	const reversedStrnig = cleanedString.split("").reverse().join("");
+
+	return cleanedString === reversedStrnig;
 };
+
+console.log(palindromes("A car, a man, a maraca"));
 
 // Do not edit below this line
 module.exports = palindromes;
