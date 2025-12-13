41db473696b265f1706b0aff89e23b9d66775f46
Giuliana Villarroel <giu.villarroel@gmail.com>
2025-12-13 20:49:00 -0300

    Complete findTheOldest function

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const getAge = (yearOfBirth, yearOfDeath) => {
+	if (!yearOfDeath) {
+		yearOfDeath = new Date().getFullYear();
+	}
+	return yearOfDeath - yearOfBirth;
+};
 
+const findTheOldest = function (peopleArray) {
+	return peopleArray.reduce((oldest, currentPerson) => {
+		const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+		const youngerAge = getAge(
+			currentPerson.yearOfBirth,
+			currentPerson.yearOfDeath
+		);
+
+		return oldestAge > youngerAge ? oldest : currentPerson;
+	});
 };
 
 // Do not edit below this line
