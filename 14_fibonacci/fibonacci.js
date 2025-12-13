9471ec3b4a231898b8b590bd830b4a4e083f6463
Giuliana Villarroel <giu.villarroel@gmail.com>
2025-12-13 18:01:53 -0300

    Complete fibonacci function

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function (position) {
+	let index = typeof position !== "number" ? parseInt(position) : position;
+	if (index < 0) return "OOPS";
+	if (index === 0) return index;
 
+	const sequence = [0, 1];
+	for (let currentIndex = 2; currentIndex <= index; currentIndex++) {
+		sequence[currentIndex] =
+			sequence[currentIndex - 1] + sequence[currentIndex - 2];
+	}
+
+	return sequence[index];
 };
+
+console.log(fibonacci(4));
 
 // Do not edit below this line
 module.exports = fibonacci;
