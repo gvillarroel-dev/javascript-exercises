7cf76a9ad66ce4953e38b08f008c2415245b7e0c
Villarroel Giuliana <giu.villarroel@gmail.com>
2026-07-25 13:02:53 -0300

    Complete factorial function

--- computer_science/recursion/1_factorial/factorial.js
+++ computer_science/recursion/1_factorial/factorial.js
@@ -1,5 +1,12 @@
-const factorial = function() {
+const factorial = function (num) {
+	if (num < 0 || !Number.isInteger(num)) {
+		return;
+	}
+	if (num === 0 || num === 1) {
+		return 1;
+	}
 
+	return num * factorial(num - 1);
 };
 
 // Do not edit below this line
