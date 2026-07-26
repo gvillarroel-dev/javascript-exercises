100edb4fdf5e63851ca1964a8df78df72a895823
Villarroel Giuliana <giu.villarroel@gmail.com>
2026-07-26 20:36:42 -0300

    Complete contains function

--- computer_science/recursion/2_contains/contains.js
+++ computer_science/recursion/2_contains/contains.js
@@ -1,6 +1,19 @@
-const contains = function() {
-  
+const contains = function (object, searchValue) {
+	for (const key in object) {
+		const value = object[key];
+        
+		if (Object.is(value, searchValue)) {
+			return true;
+		}
+
+        if (typeof value == "object" && value !== null) {
+            if (contains(value, searchValue)) {
+                return true;
+            }
+        }
+	}
+    return false;
 };
-  
+
 // Do not edit below this line
 module.exports = contains;
