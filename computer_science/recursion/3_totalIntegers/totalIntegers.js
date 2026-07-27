bf41f4b39691134116e97c86b65eb3a937227a1a
Villarroel Giuliana <giu.villarroel@gmail.com>
2026-07-27 13:04:03 -0300

    Complete totalIntegers function

--- computer_science/recursion/3_totalIntegers/totalIntegers.js
+++ computer_science/recursion/3_totalIntegers/totalIntegers.js
@@ -1,6 +1,20 @@
-const totalIntegers = function() {
-  
+const totalIntegers = function (object) {
+    if (typeof object !== "object" || object === null) return;
+    
+    let accumulator = 0;
+    for (const key in object) {
+        const value = object[key];
+        
+        if (Number.isInteger(value)) {
+            accumulator++;
+        }
+        
+        if (typeof value === "object" && value !== null) {
+            accumulator += totalIntegers(value);
+        }
+    }
+    return accumulator;
 };
-  
+
 // Do not edit below this line
 module.exports = totalIntegers;
