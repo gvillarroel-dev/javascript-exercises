b4fb5116423d55d6a5f27b6126454278c25b66d8
Villarroel Giuliana <giu.villarroel@gmail.com>
2026-07-27 13:49:58 -0300

    Complete permutations function

--- computer_science/recursion/4_permutations/permutations.js
+++ computer_science/recursion/4_permutations/permutations.js
@@ -1,5 +1,18 @@
-const permutations = function() {
-  
+const permutations = function(array, currentPosition  = 0, allPermutations = []) {
+    // base case
+    if (currentPosition === array.length) {
+        allPermutations.push([...array]);
+        return allPermutations;
+    }
+
+    // recursion case
+    for (let candidateIndex = currentPosition; candidateIndex < array.length; candidateIndex++) {
+        [array[currentPosition], array[candidateIndex]] = [array[candidateIndex], array[currentPosition]];
+        
+        permutations(array, currentPosition + 1, allPermutations);
+        [array[candidateIndex], array[currentPosition]] = [array[currentPosition], array[candidateIndex]];
+    }
+    return allPermutations;
 };
   
 // Do not edit below this line
