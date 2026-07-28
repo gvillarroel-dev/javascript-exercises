a8d50e42955335986e3427de8dbe08ae2ceec7d2
Villarroel Giuliana <giu.villarroel@gmail.com>
2026-07-27 21:02:42 -0300

    Complete pascal function

--- computer_science/recursion/5_pascal/pascal.js
+++ computer_science/recursion/5_pascal/pascal.js
@@ -1,6 +1,21 @@
-const pascal = function() {
-  
+const pascal = function (counter) {
+    if (counter < 1 || !Number.isInteger(counter)) return;
+    if (counter === 1) {
+        return [1];
+    }
+
+    const previousLine = pascal(counter - 1);
+    const newLine = [];
+    for (let i = 0; i < counter; i++) {
+        let value = previousLine[i - 1] + previousLine[i];
+        if (Number.isNaN(value)) {
+            value = 1;
+        
+        }
+        newLine.push(value);
+    }
+    return newLine;
 };
-  
+
 // Do not edit below this line
 module.exports = pascal;
